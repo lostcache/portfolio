16302217e33445b4dcf659db57faed0cbf9b3756
Harsh Devisha <harshdevisha9111@gmail.com>
2025-01-09 12:59:06 -0800

    refactor

--- script.js
+++
@@ -1,8 +0,0 @@
-function showTab(tabId) {
-  const contents = document.querySelectorAll(".tab-content");
-  contents.forEach((content) => {
-    content.style.display = "none";
-  });
-
-  document.getElementById(tabId).style.display = "block";
-}
